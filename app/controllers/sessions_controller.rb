c5c506b558d07e2eb7c0eaa404642df7adf167f1
johnmccann1991 <john.mccann@kellogg.northwestern.edu>
2022-02-22 16:05:39 +0000

    First save

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+
+    def new
+
+
+    def create
+        entered_email = params["email"]
+        entered_password = params ["password"]
+        @user = User.find_by({email: entered_email})
+        if @user
+            if BCrypt::Password.new(@user.password) == entered_password
+                redirect_to"/companies"
+        else
+            redirect_to "/sessions/new"
+        end
+    else
+        redirect_to"/sessions/new"
+    end 
+    
+
+
+
+
+
+
+
+end
